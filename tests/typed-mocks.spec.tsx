2a49a96067353f294953f6d0bb24bb42b5b6a5bc
Andrei Picus <NiGhTTraX@users.noreply.github.com>
2018-04-29 15:21:27 +0200

    Add a learning test for mocking typed functions

---
+++ tests/typed-mocks.spec.tsx
@@ -0,0 +1,25 @@
+import { IMock, Mock } from 'typemoq';
+
+describe('Learning typed mocks', function () {
+  it('should support mocking typed functions', () => {
+    interface SomeArguments {
+      foo: number;
+      bar: string;
+    }
+
+    type DoTheThing = (opts: SomeArguments) => string;
+
+    const mock: IMock<DoTheThing> = Mock.ofType<DoTheThing>();
+
+    mock.setup((fakeDoTheThing: DoTheThing) => fakeDoTheThing({
+      foo: 2,
+      bar: 'bar'
+    })).returns(({ foo, bar }) => foo + bar).verifiable();
+
+    const fakeDoTheThing: DoTheThing = mock.object;
+
+    fakeDoTheThing({ foo: 2, bar: 'bar' });
+
+    mock.verifyAll();
+  });
+});
